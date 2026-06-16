620e62d3cf718448a0b608eb13707eaac77e99f7
VarunKumar123456 <chitikenavarunkumar@gmail.com>
2026-06-16 16:57:32 +0530

    prepare frontend for vercel

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,28 +1,23 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://127.0.0.1:8000",
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
-  console.log(
-    "➡️ REQUEST URL:",
-    config.url
-  );
-
-  console.log(
-    "➡️ RAW TOKEN:",
-    token
-  );
+  console.log("➡️ REQUEST URL:", config.url);
+  console.log("➡️ RAW TOKEN:", token);
 
   if (
     token &&
-    config.url !== "/auth/login"
+    config.url !== "/auth/login" &&
+    config.url !== "/auth/register"
   ) {
-    config.headers.Authorization =
-      `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
